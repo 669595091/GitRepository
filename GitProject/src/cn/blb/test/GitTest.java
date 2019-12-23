9ec747e2b391b593620cc4d114e032d746b1a103
669595091 <669595091@qq.com>
2019-12-23 19:57:11 +0800

    woshinibaba

--- GitProject/src/cn/blb/test/GitTest.java
+++ GitProject/src/cn/blb/test/GitTest.java
@@ -4,9 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("֣���Ҷ��ӣ���");
-		System.out.println("֣��say:��������Ĵ����,����ָ�̣�");
-		System.out.println("֣��say:'û���Ҿ������Ķ��ӣ�������ب�Ų���ģ���' ");
+
 		System.out.println("֣ssssssssssssss");
 	}
 
