c28dca7091dfd8313640dfcd70d217a7d6e3513e
669595091 <669595091@qq.com>
2019-12-23 17:28:38 +0800

    沙雕

--- GitProject/src/cn/blb/test/GitTest.java
+++ GitProject/src/cn/blb/test/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("郑超我儿子！！");
+		System.out.println("郑超say:没错！我就是杨大大的二儿子！有何指教？ ");
 		System.out.println("郑超say:'没错！我就是他的儿子！！这是亘古不变的！！' ");
 	}
 
